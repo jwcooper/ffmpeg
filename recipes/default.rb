7b39336bf9f0cdc978ce2c4c737460398e6bb5c4
Justin Cooper <jwcooper@gmail.com>
2016-07-18 16:38:16 -0500

    Use to_sym to allow strings in attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright 2014, Escape Studios
 #
 
-case node['ffmpeg']['install_method']
+case node['ffmpeg']['install_method'].to_sym
 when :source
   include_recipe 'ffmpeg::source'
 when :package
